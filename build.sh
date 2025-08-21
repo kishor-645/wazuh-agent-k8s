aa97943bfd61db7069845de87eea08c7bbfff609
kishor vaishanav <75803767+kishor-645@users.noreply.github.com>
2025-08-21 13:35:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build --no-cache -t kishor645/wazuh-agent:$1 .
-docker push kishor645/wazuh-agent:$1
+docker build --no-cache -t your-registry.azurecr.io/wazuh-agent:$1 .
+docker push your-registry.azurecr.io/wazuh-agent:$1
 kubectl apply -f k8s/
